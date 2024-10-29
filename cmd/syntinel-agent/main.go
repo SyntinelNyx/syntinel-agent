d327c54849293dc300887f7e2265ce4b6443c6be
Rogue-King <miguel@muniz.org>
2024-10-29 12:42:02 -0700

    Added Sysinfo. TODO: -Need to find a library for realtime process -slim down query -talk to backend with websocket

---
+++ cmd/syntinel-agent/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/SyntinelNyx/syntinel-agent/internal/sysinfo"
+
+func main() {
+	sysinfo.Sysinfo()
+}
---
+++ internal/sysinfo/sysinfo.go
@@ -0,0 +1,32 @@
+package sysinfo
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/user"
+
+	"github.com/zcalusic/sysinfo"
+)
+
+func Sysinfo() {
+	current, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if current.Uid != "0" {
+		log.Fatal("requires superuser privilege")
+	}
+
+	var si sysinfo.SysInfo
+
+	si.GetSysInfo()
+
+	data, err := json.MarshalIndent(&si, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(data))
+}
