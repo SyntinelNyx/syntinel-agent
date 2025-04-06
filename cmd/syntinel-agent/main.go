16bbaf5a286999d4d5ac8d453b79b8dbf1e77285
rogueking <miguel@muniz.org>
2025-04-05 19:06:14 -0700

    fixed git ignore

--- cmd/syntinel-agent/main.go
+++ cmd/syntinel-agent/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	// "github.com/SyntinelNyx/syntinel-agent/internal/grpc"
+	"github.com/SyntinelNyx/syntinel-agent/internal/grpc"
 	"github.com/SyntinelNyx/syntinel-agent/internal/trivy"
 )
 
 func main() {
-	// client := grpc.InitConnectToServer()
+	client := grpc.InitConnectToServer()
 
-	// grpc.StartBidirectionalStream(client)
+	grpc.StartBidirectionalStream(client)
 
 	trivy.DeepScan("/")
 
---
+++ internal/trivy/trivy.go
@@ -0,0 +1,20 @@
+package trivy
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+//variable
+var binaryPath = "./data/dependencies/trivy"
+
+func DeepScan(path string) {
+	// Execute the binary
+	cmd := exec.Command(binaryPath, "fs", "-f", "json", "--scanners", "vuln", path)
+	// fmt.Println("Executing command:", cmd.String())	
+	output, err := cmd.CombinedOutput() // Captures stdout and stderr
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(string(output))
+}
