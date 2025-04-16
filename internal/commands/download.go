49bed19b46b65672d42279e53de74b33c9113c6d
SapphireGaze <jasmine@sapphiregaze.dev>
2025-04-15 23:31:56 -0700

    Fixed File Streaming

--- internal/commands/download.go
+++ internal/commands/download.go
@@ -1,24 +1,29 @@
 package commands
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 	"path/filepath"
 
-    "github.com/SyntinelNyx/syntinel-agent/internal/logger"
+	"github.com/SyntinelNyx/syntinel-agent/internal/logger"
 )
 
 func DownloadFile(name string, data []byte) string {
 	path := filepath.Join("/etc", "syntinel", name)
 
-    err := os.WriteFile(path, data, 0644)
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		logger.Error("Failed to open file %s: %v", path, err)
+		return fmt.Sprintf("Failed to open file %s: %v", path, err)
+	}
+	defer file.Close()
 
-    if err != nil {
-        logger.Errorf("Failed to write file %s: %v", path, err)
-        return fmt.Sprintf("Failed to write file %s: %v", path, err)
-    }
+	_, err = file.Write(data)
+	if err != nil {
+		logger.Error("Failed to write to file %s: %v", path, err)
+		return fmt.Sprintf("Failed to write to file %s: %v", path, err)
+	}
 
-    logger.Infof("File %s downloaded successfully", name)
-
-    return fmt.Sprintf("File %s downloaded successfully", name)
+	logger.Info("Chunk written to file %s successfully", name)
+	return fmt.Sprintf("Chunk written to file %s successfully", name)
 }
--- internal/commands/exec.go
+++ internal/commands/exec.go
@@ -1,55 +1,55 @@
 package commands
 
 import (
-    "fmt"
-    "os/exec"
+	"fmt"
+	"os/exec"
 	"strings"
 
 	"github.com/SyntinelNyx/syntinel-agent/internal/logger"
 )
 
 func RunScript(args ...string) string {
-    if len(args) == 0 {
-        fmt.Println("No arguments provided")
-        return ""
-    }
-
-    // Parse the first argument with space delimiter
-    parsedArgs := strings.Split(args[0], " ")
-
-    scriptPath := "/etc/syntinel/" + parsedArgs[0]
-    parsedArgs = parsedArgs[1:] // Remove the script name from the arguments
-
-    // Execute the script
-    cmd := exec.Command("bash", append([]string{scriptPath}, parsedArgs...)...)
-    output, err := cmd.CombinedOutput() // Captures stdout and stderr
-    if err != nil {
-        logger.Error("Error executing script: %v", err)
-    }
-	logger.Info("Script output: %s", string(output))
-
-    return string(output)
-}
-
-func RunBinary(args ...string) string{
-    if len(args) == 0 {
-        logger.Error("No arguments provided")
-        return ""
-    }
+	if len(args) == 0 {
+		fmt.Println("No arguments provided")
+		return ""
+	}
 
 	// Parse the first argument with space delimiter
-    parsedArgs := strings.Split(args[0], " ")
+	parsedArgs := strings.Split(args[0], " ")
 
-    binaryPath := "/etc/syntinel/" + parsedArgs[0]
-    parsedArgs = parsedArgs[1:] // Remove the script name from the arguments
+	scriptPath := "/etc/syntinel/" + parsedArgs[0]
+	parsedArgs = parsedArgs[1:] // Remove the script name from the arguments
 
-    // Execute the binary
-    cmd := exec.Command(binaryPath, parsedArgs...)
-    output, err := cmd.CombinedOutput() // Captures stdout and stderr
-    if err != nil {
-        logger.Error("Error:", err)
-    }
-    logger.Info(string(output))
+	// Execute the script
+	cmd := exec.Command("bash", append([]string{scriptPath}, parsedArgs...)...)
+	output, err := cmd.CombinedOutput() // Captures stdout and stderr
+	if err != nil {
+		logger.Error("Error executing script: %v", err)
+	}
+	logger.Info("Script output: %s", string(output))
 
-    return string(output)
+	return string(output)
+}
+
+func RunBinary(args ...string) string {
+	if len(args) == 0 {
+		logger.Error("No arguments provided")
+		return ""
+	}
+
+	// Parse the first argument with space delimiter
+	parsedArgs := strings.Split(args[0], " ")
+
+	binaryPath := "/etc/syntinel/" + parsedArgs[0]
+	parsedArgs = parsedArgs[1:] // Remove the script name from the arguments
+
+	// Execute the binary
+	cmd := exec.Command(binaryPath, parsedArgs...)
+	output, err := cmd.CombinedOutput() // Captures stdout and stderr
+	if err != nil {
+		logger.Error("Error:", err)
+	}
+	logger.Info(string(output))
+
+	return string(output)
 }
