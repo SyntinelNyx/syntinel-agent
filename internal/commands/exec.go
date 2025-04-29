48fa3082de39c5083668fbe5aacb378d1a484c81
sapphiregaze <jasmine@sapphiregaze.dev>
2025-04-29 05:02:18 -0700

    Added Home Path Expansion To Exec

--- internal/commands/exec.go
+++ internal/commands/exec.go
@@ -2,9 +2,19 @@ package commands
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
+
+func expandHome(path string) string {
+	if strings.HasPrefix(path, "~") {
+		if home, err := os.UserHomeDir(); err == nil {
+			return strings.Replace(path, "~", home, 1)
+		}
+	}
+	return path
+}
 
 func Exec(command string) (string, error) {
 	if len(command) == 0 {
@@ -12,15 +22,19 @@ func Exec(command string) (string, error) {
 	}
 
 	args := strings.Fields(command)
+	for i, arg := range args {
+		args[i] = expandHome(arg)
+	}
+
 	execPath, err := exec.LookPath(args[0])
 	if err != nil {
-		return "", fmt.Errorf("Error resolving path for %s: %v", args[0], err)
+		return "", fmt.Errorf("error resolving path for %s: %v", args[0], err)
 	}
 
 	cmd := exec.Command(execPath, args[1:]...)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("Error executing command: %v", err)
+		return "", fmt.Errorf("error executing command: %v\n%s", err, string(output))
 	}
 
 	return string(output), nil
